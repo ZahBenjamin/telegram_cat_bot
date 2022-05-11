c72e6c10593f31cb9ac4454b48c87615789f8984
ZahBenjamin <bz@benzah.com>
2022-05-11 14:15:07 -0400

    Set up API_TOKEN

---
+++ script.rb
@@ -0,0 +1,17 @@
+gem 'telegram-bot-ruby', '~>0.19.2'
+
+require 'telegram/bot'
+require '.env'
+
+token = 'API_ID'
+
+Telegram::Bot::Client.run(token) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start'
+      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
+    when '/stop'
+      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
+    end
+  end
+end
