4f164b71f456206c2b79cdecde7c5d09de9ce9fe
ZahBenjamin <bz@benzah.com>
2022-05-16 17:29:22 -0400

    Refactor

--- script.rb
+++ script.rb
@@ -4,30 +4,21 @@ puts Telegram::Bot::VERSION
 
 token = $TELEGRAM_TOKEN
 
+# Dir.glob("assets/*.{jpg,heic}")  
 
-img_arr = Dir.foreach("./assets").select { |x| File.file?("#{"./assets"}/#{x}") }
-img_arr.delete('.DS_Store')
+img_arr = Dir.glob("assets/*.{jpg,heic}")
   
 p img_arr
 p img_arr.length
-
-
-# 1. Make script to choose random image from assets folder
-# 2. Let people add the bot to their channel/group
-# 2.1 Make array with img names
-# 3. Make bot for discord too
 
 
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
     when '/photo'
       random_photo = img_arr.sample
-      # magic conversion
-      bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new("./assets/#{random_photo}",  'image/jpeg'))
+      bot.api.send_photo(chat_id: message.chat.id, photo: Faraday::UploadIO.new(random_photo,  'image/jpeg'))
     end
   end
 end
